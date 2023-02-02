da8cb6347db6a68550cc3dcfe0b7a43d007165ed
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-02 15:34:05 +0100

    etape 2

--- src/App.js
+++ src/App.js
@@ -7,68 +7,70 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConf, setPasswordConf] = useState("");
-  //state qui gère le contenu et l'affichage de mon message d'erreur
+  //state qui gère le contenu et l'affichage de mon étape 2
   const [newBox, setNewBox] = useState("");
 
   return (
     <div className="App">
       <h1>Create account</h1>
-      <form
-        className="form"
-        //onSubmit sera déclenchée lors du clic sur un bouton ou un input de type Submit
-        onSubmit={(event) => {
-          //empeche le rafriassissement de la page par défaut
-          event.preventDefault();
 
-          if (password !== passwordConf) {
-            // setErrorMessage("");
-            alert("Vos deux mots de passe ne sont pas identiques");
-          } else {
-          }
-        }}
-      >
-        <div className="tab">
-          <input
-            value={name}
-            type="text"
-            placeholder="name"
-            onChange={(event) => setName(event.target.value)}
-          />
+      <div>
+        <form
+          className="form"
+          //onSubmit sera déclenchée lors du clic sur un bouton ou un input de type Submit
+          onSubmit={(event) => {
+            //empeche le rafriassissement de la page par défaut
+            event.preventDefault();
 
-          <input
-            value={email}
-            type="email"
-            placeholder="email"
-            onChange={(event) => setEmail(event.target.value)}
-          />
+            if (password !== passwordConf) {
+              // setErrorMessage("");
+              alert("Vos deux mots de passe ne sont pas identiques");
+            } else {
+            }
+          }}
+        >
+          <div className="tab">
+            <input
+              value={name}
+              type="text"
+              placeholder="name"
+              onChange={(event) => setName(event.target.value)}
+            />
 
-          <input
-            value={password}
-            type="password"
-            placeholder="password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
+            <input
+              value={email}
+              type="email"
+              placeholder="email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
 
-          <input
-            value={passwordConf}
-            type="password"
-            placeholder="password"
-            onChange={(event) => setPasswordConf(event.target.value)}
-          />
+            <input
+              value={password}
+              type="password"
+              placeholder="password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
 
-          {/* bouton pour déclencher le submit du formulaire */}
-          <button className="register" type="submit">
-            Register
-          </button>
+            <input
+              value={passwordConf}
+              type="password"
+              placeholder="password"
+              onChange={(event) => setPasswordConf(event.target.value)}
+            />
 
-          <div className="newBox">
-            <p>Name : {name}</p>
-            <p>Email : {email}</p>
-            <p>Password : {password}</p>
+            {/* bouton pour déclencher le submit du formulaire */}
+            <button className="register" type="submit">
+              Register
+            </button>
+
+            <div className="newBox">
+              <p>Name : {name}</p>
+              <p>Email : {email}</p>
+              <p>Password : {password}</p>
+            </div>
           </div>
-        </div>
-      </form>
-
+        </form>
+      </div>
       <footer>
         <p>
           Made with <span> React </span> at <span> Le Reacteur </span> by{" "}
