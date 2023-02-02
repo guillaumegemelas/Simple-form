5cf9700a00498f23dc3b79535c1bcadf1b616b7b
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-02 16:45:34 +0100

    etape 3

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConf, setPasswordConf] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [submit, setSubmit] = useState("");
   //state qui gère le contenu et l'affichage de mon étape 2
   const [newBox, setNewBox] = useState("");
 
   return (
     <div className="App">
-      <h1>Create account</h1>
-
-      <div>
+      <section className={!submit || errorMessage ? "" : "hidden"}>
+        <h1>Create account</h1>
+        {/* renvoie à la classe visible si submit n'existe pas ou errorMessage existe et caché si
+       submit existe et erroMessage n'existe pas  */}
         <form
           className="form"
           //onSubmit sera déclenchée lors du clic sur un bouton ou un input de type Submit
@@ -23,7 +26,7 @@ function App() {
             event.preventDefault();
 
             if (password !== passwordConf) {
-              // setErrorMessage("");
+              setErrorMessage("");
               alert("Vos deux mots de passe ne sont pas identiques");
             } else {
             }
@@ -59,18 +62,41 @@ function App() {
             />
 
             {/* bouton pour déclencher le submit du formulaire */}
-            <button className="register" type="submit">
+            <button
+              className="register"
+              type="submit"
+              onClick={() => {
+                setSubmit("OK");
+              }}
+            >
               Register
             </button>
+          </div>
+        </form>
+      </section>
 
+      {submit && !errorMessage && (
+        <section className="result">
+          <div>
+            <h1>Results</h1>
             <div className="newBox">
               <p>Name : {name}</p>
               <p>Email : {email}</p>
               <p>Password : {password}</p>
             </div>
+
+            <button
+              className="register"
+              type="submit"
+              onClick={() => {
+                setSubmit("");
+              }}
+            >
+              Edit your information
+            </button>
           </div>
-        </form>
-      </div>
+        </section>
+      )}
       <footer>
         <p>
           Made with <span> React </span> at <span> Le Reacteur </span> by{" "}
